3ca303a27d4d5503367c01d15856f8725bbeb979
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-05 16:40:44 -0300

    ADT Sphere

--- inc/sphere.h
+++ inc/sphere.h
@@ -1,7 +1,7 @@
 /**
  * @file sphere.h
  * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
- * @brief TDA de esferas
+ * @brief TDA Esfera
  * @version 1.0
  * @date 2021-12-01
  *
@@ -9,35 +9,50 @@
  *
  */
 
+#ifndef SPHERE_H
+#define SPHERE_H
+
 #include "vector.h"
 
 /* ----------------------------- Data structures ---------------------------- */
-
 typedef struct {
-    vector_t centro;
-    float radio;
+	vector_t    centro;
+	float       radio;
 } esfera_t;
 
 /* ------------------------------- Prototypes ------------------------------- */
 
-//TODO Agregar a grupo con @defgroup o addgroup
+// TODO Agregar a grupo con @defgroup o addgroup
 
 /**
- * @brief  Creacion de una estructura esfera_t en memoria dinamica
- * @note   Los parametros que recibe la funcion son de tipo read-only.
- * @param  centro: Struct vector_t cuyo miembro es una terna ordenada. Centro de la esfera
- * @param  radio: Radio de la esfera
- * @param  color: Struct color_t cuyo miembro es una terna ordenada del tipo RGB. Color ambiente
- * @param  ka: Coeficiente de reflexion ambiental
- * @param  kd: Coeficiente de reflexion difusa
- * @retval P: Puntero a la estructura esfera_t creada
- * @retval NULL: Error en la creacion de la estructura esfera_t
+ * @brief       Constructor de esfera_t (crea una esfera a partir de un centro y un radio en memoria dinámica)
+ * @param[in]   centro: Centro de la esfera
+ * @param[in]   radio: Radio de la esfera
+ * @retval      esfera_t*: Puntero a la estructura esfera_t creada
+ * @retval      NULL: Error en la creacion de la estructura esfera_t
  */
-esfera_t *esfera_crear(const vector_t centro, const float radio);
+esfera_t        *esfera_crear(const vector_t centro, const float radio);
 
 /**
- * @brief  Libera la memoria dinamica de una estructura esfera_t
- * @param  *esfera_: Puntero a la estructura esfera_t a liberar
- * @retval None
+ * @brief       Destructor de la estructura esfera_t reservada en memoria dinamica
+ * @param[in]   esfera: Puntero a la estructura esfera_t a destruir
+ * @retval      None
  */
-void esfera_destruir(esfera_t *esfera);
+void            esfera_destruir(esfera_t *esfera);
+
+/**
+ * @brief       Computa la distancia a la que se encuentra desde un determinado origen el
+ *              primer impacto de un rayo sobre una esfera
+ * @pre         La esfera debe ser un puntero a una estructura esfera_t valida
+ * @pre         El rayo d debe ser un versor.
+ * @param[in]   esfera: Puntero a struct esfera_t.
+ * @param[in]   o: Coordenadas de origen desde las que el rayo es disparado
+ * @param[in]   d: Versor asociado al rayo a disparar contra la esfera
+ * @param[out]  punto: Punto de impacto.
+ * @param[out]  normal: Normal de la esfera en el punto de impacto.
+ * @retval      float: Distancia calculada
+ * @retval      INFINITO: El rayo no impacto sobre la esfera
+ */
+float           esfera_distancia(const esfera_t *esfera, const vector_t o, const vector_t d, vector_t *punto, vector_t *normal);
+
+#endif  // SPHERE_H
--- src/core/shapes/sphere.c
+++ src/core/shapes/sphere.c
@@ -1,7 +1,7 @@
 /**
  * @file sphere.c
  * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
- * @brief TDA de esferas
+ * @brief TDA Esfera
  * @version 1.0
  * @date 2021-12-05
  *
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 
 #include "../../../inc/sphere.h"
+#include "../../../inc/vector.h"
 
 esfera_t *esfera_crear(const vector_t centro, const float radio) {
 
@@ -27,4 +28,27 @@ esfera_t *esfera_crear(const vector_t centro, const float radio) {
 
 void esfera_destruir(esfera_t *esfera) {
 	free(esfera);
+}
+
+float esfera_distancia(const esfera_t *esfera, const vector_t o, const vector_t d, vector_t *punto, vector_t *normal) {
+
+	const vector_t co = vector_resta(esfera->centro, o);
+	const float cd = vector_producto_interno(co, d);
+	const float delta = cd * cd - vector_producto_interno(co, co) + esfera->radio * esfera->radio;
+
+	if ((delta >= 0) && (cd + sqrt(delta) > 0)) {	 // Corto circuito. Priorizo delta >= 0.
+
+		const float sqrtDelta = sqrt(delta);
+		const float t = cd - sqrtDelta > 0 ? cd - sqrtDelta : cd + sqrtDelta;
+
+		if (punto != NULL && normal != NULL) {
+			*punto = vector_interpolar_recta(o, d, t);
+			*normal = vector_resta(*punto, esfera->centro);
+			normal->x /= esfera->radio;
+			normal->y /= esfera->radio;
+			normal->z /= esfera->radio;
+		}
+		return t;
+	}
+	return INFINITO;
 }
