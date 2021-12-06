5de577a3ebebde245829fcca98a6c5f1ee31899e
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-06 17:56:36 -0300

    Create triangle.c

---
+++ src/core/shapes/triangle.c
@@ -0,0 +1,20 @@
+/**
+ * @file triangle.c
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief TDA Triangulo
+ * @version 1.0
+ * @date 2021-12-06
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+//TODO Implementar el TDA Triangulo
+
+#include "../../../inc/triangle.h"
+
+struct triangulo {
+    vector_t p1;
+    vector_t p2;
+    vector_t p3;
+};
