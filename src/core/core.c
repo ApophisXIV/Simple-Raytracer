e12d49edbf5184459f3d49438bb9768238003c1f
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-06 18:16:45 -0300

    Update core.c

--- src/core/core.c
+++ src/core/core.c
@@ -1,6 +1,7 @@
 /**
  * @file core.c
  * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief App core
  * @version 1.0
  * @date 2021-11-19
  *
@@ -10,27 +11,36 @@
 
 #include <math.h>
 
-#include "../../include/core.h"
+#include "../../inc/core.h"
+#include "../../inc/dynamic_array.h"
+#include "../../inc/vector.h"
 
+/* -------------------------------- Constants ------------------------------- */
 #define PI 3.1415927f
 #define FOV 90
 
-bool fill_img_w_shape(imagen_t *img, size_t ancho, size_t alto, color_t (*figura)(color_t, vector_t, vector_t)) {
+color_t computar_intensidad(int profundidad, const arreglo_t *objetos, const arreglo_t *luces, color_t ambiente, vector_t o, vector_t d) {
+
+	// TODO Implementar
+}
+
+bool generar_escena(escena_t *escena, imagen_t *imagen) {
 
 	const color_t ambiente = {.05, .05, .05};
 	const vector_t origen = {0, 0, 0};
 
-	const float z = ancho / 2 / tan(FOV / 2 * PI / 180);
+	const float z = escena->ancho / 2 / tan(FOV / 2 * PI / 180);
 
-	const int y_max = alto / 2;
-	const int x_max = ancho / 2;
+	const int y_max = escena->alto / 2;
+	const int x_max = escena->ancho / 2;
 
 	for (int y = y_max; y > -y_max; y--)
 		for (int x = -x_max; x < x_max; x++) {
 
-			const color_t c = figura(ambiente, origen, vector_normalizar((vector_t){x, y, z}));
+            // TODO Testear
+			const color_t c = computar_intensidad(escena->iteraciones, escena->objetos, escena->luces, ambiente, origen, vector_normalizar((vector_t){x, y, z}));
 
-			if (!imagen_set_pixel(img, x + x_max, y + y_max - 1, c))
+			if (!imagen_set_pixel(imagen, x + x_max, y + y_max - 1, c))
 				return false;
 		}
 
