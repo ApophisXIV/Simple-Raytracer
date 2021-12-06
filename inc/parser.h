23f30fb75577115fc44b7856f32fc00986825144
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-06 18:16:28 -0300

    Update parser.h

--- inc/parser.h
+++ inc/parser.h
@@ -1,6 +1,7 @@
 /**
  * @file parser.h
  * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief Parser de entrada de datos y escena
  * @version 1.0
  * @date 2021-11-19
  *
@@ -15,22 +16,23 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "scene.h"
+
 /* ---------------------------------- Enums --------------------------------- */
-enum FILE_TYPE {
+typedef enum {
     FT_BMP,
     FT_PPM
-};
+} file_type_t;
 
 /* ------------------------------- Prototypes ------------------------------- */
 /**
  * @brief   Verifica si la entrada de datos es valida.
  * @param   argc: Cantidad de argumentos pasados por linea de comandos.
  * @param   *argv[]: Puntero a un arreglo de punteros a cadenas de caracteres pasados por linea de comandos.
- * @param   *ancho: Puntero a un entero donde se almacenara el ancho de la imagen.
- * @param   *alto: Puntero a un entero donde se almacenara el alto de la imagen.
+ * @param   *scene: Puntero a una estructura de tipo scene_t.
  * @param   *extension: Puntero a un entero donde se almacenara el tipo de archivo.
  * @retval  bool: True si la entrada es valida, false en caso contrario.
  */
-bool is_a_valid_input(int argc, char *argv[], int *ancho, int *alto, int *extension);
+bool is_a_valid_input(int argc, char *argv[], escena_t *escena, file_type_t *extension);
 
 #endif // PARSER_H
