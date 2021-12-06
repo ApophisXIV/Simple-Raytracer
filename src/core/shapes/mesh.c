e4f4e40f4b4a7af0301427c95c03d353f63f7f8e
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-06 17:27:06 -0300

    Create mesh.c

---
+++ src/core/shapes/mesh.c
@@ -0,0 +1,14 @@
+/**
+ * @file mesh.c
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief TDA Malla
+ * @version 1.0
+ * @date 2021-12-06
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+//TODO Implementar el TDA Malla
+
+#include "../../../inc/mesh.h"
