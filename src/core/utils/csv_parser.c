8696d9ddaa1e4b7583d92a9dbb9359a14f1f480e
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-12-06 17:23:55 -0300

    Create csv_parser.c

---
+++ src/core/utils/csv_parser.c
@@ -0,0 +1,13 @@
+
+/**
+ * @file csv_parser.c
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief CSV parser
+ * @version 1.0
+ * @date 2021-12-06
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "../../../inc/csv_parser.h"
