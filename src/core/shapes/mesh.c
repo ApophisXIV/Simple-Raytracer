d8a037b352538f5a3407ff4475c285f97c5b42c1
JohyPol <jpollero@fi.uba.ar>
2021-12-08 11:21:48 -0300

    Started Implementation of TDA mesh

--- src/core/shapes/mesh.c
+++ src/core/shapes/mesh.c
@@ -12,8 +12,30 @@
 // TODO Implementar el TDA Malla
 
 #include "../../../inc/mesh.h"
+#include "../../../inc/dynamic_array.h"
+#include "../../../inc/triangle.h"
+
+#include <stdio.h>
+
+// TODO Ver de como seria la estructura de malla
 
 struct malla {
-
-    // TODO Ver de como seria la estructura de malla
+    arreglo_t **v;
+    size_t n;
 };
+
+struct malla2 {
+    triangulo_t *p
+    
+};
+
+malla_t *malla_crear(const char *archivo_stl){
+    FILE *p = fopen("archivo.stl", "rb");
+    if(p == NULL) return NULL;
+}
+
+/*
+Es posible construir una malla como un arreglo de triángulos, donde la distancia 
+estará dada por la intersección más cercana entre el rayo y todos 
+los triángulos de la misma y la normal será la de ese triángulo.
+*/
