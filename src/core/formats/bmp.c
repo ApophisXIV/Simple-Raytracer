bf4969314de20c231ff53adca713e841963af376
JohyPol <jpollero@fi.uba.ar>
2021-12-10 17:28:29 -0300

    Updated bmp.c

--- src/core/formats/bmp.c
+++ src/core/formats/bmp.c
@@ -12,7 +12,40 @@
 #include <stdint.h>
 
 #include "../../../inc/bmp.h"
-#include "../../../inc/endianess.h"
+
+/**
+ * @brief Escritura de 2 bytes en formato little endian
+ * @pre   El archivo debe estar abierto
+ * @param  *f: Descriptor de archivo
+ * @param  v: Valor a escribir
+ * @retval None
+ */
+static void escribir_int16_little_endian(FILE *f, int16_t v) {
+
+    uint8_t v_little_endian[2];
+    v_little_endian[0] = (uint8_t)v;
+    v_little_endian[1] = (uint8_t)(v >> 8);
+
+    fwrite(v_little_endian, sizeof(uint8_t), 2, f);
+}
+
+/**
+ * @brief Escritura de 4 bytes en formato little endian
+ * @pre   El archivo debe estar abierto
+ * @param  *f: Descriptor de archivo
+ * @param  v: Valor a escribir
+ * @retval None
+ */
+static void escribir_int32_little_endian(FILE *f, int32_t v) {
+
+    uint8_t v_little_endian[4];
+    v_little_endian[0] = (uint8_t)v;
+    v_little_endian[1] = (uint8_t)(v >> 8);
+    v_little_endian[2] = (uint8_t)(v >> 16);
+    v_little_endian[3] = (uint8_t)(v >> 24);
+
+    fwrite(v_little_endian, sizeof(uint8_t), 4, f);
+}
 
 /* ------------------------ Encabezado de Archivo BMP ----------------------- */
 #define BMP_FILE_HEADER_TYPE 0x4D42    //"BM"
