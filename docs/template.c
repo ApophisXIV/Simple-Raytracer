f56f44757cc23852fbe259fa6ed22c6d1eb5ab86
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-04 20:58:01 -0300

    Add file template. Header and source

---
+++ docs/template.c
@@ -0,0 +1,37 @@
+/**
+ * @file archivo.c
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief Descripcion
+ * @version 1.0
+ * @date YYYY-MM-DD
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+/** @addtogroup Grupo
+ *  @{
+ */
+
+/* -------------------------------- Includes -------------------------------- */
+
+
+/* ---------------------- Private constants and macros ---------------------- */
+
+
+/* ------------------------------ Private enums ----------------------------- */
+
+
+/* ------------------------- Private data structures ------------------------ */
+
+
+/* ---------------------------- Private typedefs ---------------------------- */
+
+
+/* -------------------- Private prototypes implementation ------------------- */
+
+
+/* -------------------- Public prototypes implementation -------------------- */
+
+
+/** @} */
---
+++ docs/template.h
@@ -0,0 +1,45 @@
+/**
+ * @file archivo.h
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief Descripcion
+ * @version 1.0
+ * @date YYYY-MM-DD
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+/** @addtogroup Grupo
+ *  @{
+ */
+
+/** @defgroup SubGrupo SubGrupo
+ *  Lista de funciones primitivas de SubGrupo
+ *  @{
+ */
+
+#ifndef ARCHIVO_H
+#define ARCHIVO_H
+
+/* -------------------------------- Includes -------------------------------- */
+
+
+/* ----------------------- Public constants and macros ---------------------- */
+
+
+/* ------------------------------ Public enums ------------------------------ */
+
+
+/* ------------------------- Public data structures ------------------------- */
+
+
+/* ----------------------------- Public typedefs ---------------------------- */
+
+
+/* ---------------------------- Public prototypes --------------------------- */
+
+
+/** @} */
+/** @} */
+
+#endif
