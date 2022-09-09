9e59b95eca49e6ebe9b7e605ba6d51dc5d495faa
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-09 13:41:58 -0300

    Implemented vector_invertir and vector_random

--- src/core/shapes/vector.c
+++ src/core/shapes/vector.c
@@ -9,10 +9,18 @@
  *
  */
 
-#include <math.h>
+/** @addtogroup Algebra
+ *  @{
+ */
 
+/* -------------------------------- Includes -------------------------------- */
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "../../../inc/vector.h"
 
+/* -------------------- Public prototypes implementation -------------------- */
 float vector_producto_interno(const vector_t a, const vector_t b) {
 	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
@@ -34,12 +42,30 @@ vector_t vector_resta(const vector_t a, const vector_t b) {
 	return (vector_t){a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-vector_t vector_producto_vectorial(const vector_t a, const vector_t b){
-	vector_t prod_cruz;
-	prod_cruz.x = a.y * b.z - a.z * b.y;
-	prod_cruz.y = a.x * b.z - a.z * b.x;
-	prod_cruz.z = a.x * b.y - a.y * b.x;
-
-	return prod_cruz;
+vector_t vector_producto_vectorial(const vector_t a, const vector_t b) {
+	return (vector_t){
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x};
 }
 
+vector_t vector_invertir(const vector_t a) {
+	return (vector_t){-a.x, -a.y, -a.z};
+}
+
+vector_t vector_random(const vector_t min, const vector_t max) {
+
+	static bool seed_esta_inicializado = false;
+	if (!seed_esta_inicializado) {
+		srand(time(NULL));
+		seed_esta_inicializado = true;
+	}
+
+	return (vector_t){
+		(float)rand() / RAND_MAX * (max.x - min.x) + min.x,
+		(float)rand() / RAND_MAX * (max.y - min.y) + min.y,
+		(float)rand() / RAND_MAX * (max.z - min.z) + min.z
+    };
+}
+
+/** @} */
