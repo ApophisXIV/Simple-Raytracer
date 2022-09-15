0379ec606969ac51bfa0196e526b5e178cf5404d
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-14 23:19:58 -0300

    Refactor and Documentation

--- inc/mesh.h
+++ inc/mesh.h
@@ -9,25 +9,61 @@
  *
  */
 
-// TODO Agregar documentacion
-//? TODO Revisar
+/** @addtogroup Shapes
+ *  @{
+ */
+
+/** @defgroup Poligonal_mesh Poligonal mesh
+ *  Lista de funciones primitivas de una malla
+ *  @{
+ */
 
 #ifndef MESH_H
 #define MESH_H
 
+/* -------------------------------- Includes -------------------------------- */
 #include <stddef.h>
-
 #include "triangle.h"
 #include "vector.h"
+#include "dynamic_array.h"
 
+/* ----------------------------- Public typedefs ---------------------------- */
+typedef arreglo_t malla_t;
 
-struct malla;
-typedef struct malla malla_t;
+/* ---------------------------- Public prototypes --------------------------- */
+/**
+ * @brief       Crea una malla vacia
+ * @param[in]   *archivo_stl: Nombre del archivo .stl
+ * @retval      malla_t*: Malla vacia
+ */
+malla_t        *malla_crear(const char *archivo_stl);
 
-malla_t *malla_crear(const char *archivo_stl);
+/**
+ * @brief       Destruye una malla
+ * @pre         Malla debe ser un puntero a una estructura malla_t valida
+ * @param[in]   *malla: Puntero a la estructura malla_t a destruir
+ * @retval      None
+ */
+void            malla_destruir(void *malla);
 
-void malla_destruir(malla_t *malla);
+/**
+ * @brief       Computa la distancia a la que está un punto de un triangulo de la malla desde el
+ *              origen a partir del impacto de un rayo
+ * @pre         Malla debe ser un puntero a una estructura malla_t valida
+ * @pre         El rayo d debe ser un versor.
+ * @param[in]   *malla: Puntero a una estructura malla_t
+ * @param[in]   o: Coordenadas de origen desde las que el rayo es disparado
+ * @param[in]   d: Versor asociado al rayo a disparar contra la malla
+ * @param[out]  *punto: Punto de impacto.
+ * @param[out]  *normal: Normal del tringulo sobre la que impacto el rayo.
+ * @retval      float: Distancia calculada
+ * @retval      INFINITO: El rayo no impacto sobre la malla
+ */
+float           malla_distancia(const void *malla, const vector_t o,
+				const vector_t d, vector_t * punto,
+				vector_t * normal);
 
-float malla_distancia(const malla_t *malla, const vector_t o, const vector_t d, vector_t *punto, vector_t *normal);
+/** @} */
+/** @} */
 
-#endif	  // MESH_H
+#endif				// MESH_H
