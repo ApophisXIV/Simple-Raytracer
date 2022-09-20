f27efa3669d01e9e5f0e79862e4b236119eeaccc
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-20 02:21:25 -0300

    Refactor and fix in Möller–Trumbore algorithm

--- src/core/shapes/triangle.c
+++ src/core/shapes/triangle.c
@@ -9,25 +9,30 @@
  *
  */
 
-//TODO Implementar el TDA Triangulo
+/** @addtogroup Triangle
+ *  @{
+ */
 
-#include <stdlib.h>
+/* -------------------------------- Includes -------------------------------- */
 #include <stdbool.h>
-
+#include <stdlib.h>
 #include "../../../inc/triangle.h"
 
+/* ---------------------- Private constants and macros ---------------------- */
+#define EPSILON 10E-4f
+
+/* ------------------------- Private data structures ------------------------ */
 struct triangulo {
-    vector_t p1;
-    vector_t p2;
-    vector_t p3;
-    vector_t normal;
+	vector_t p1;
+	vector_t p2;
+	vector_t p3;
+	vector_t normal;
 };
 
-#define EPSILON 0.0000001f
+/* -------------------- Public prototypes implementation -------------------- */
+triangulo_t *triangulo_crear(const vector_t p1, const vector_t p2, const vector_t p3, vector_t *nm) {
 
-triangulo_t *triangulo_crear(const vector_t p1, const vector_t p2, const vector_t p3){
-
-    triangulo_t *triangulo_ptr = malloc(sizeof(triangulo_t));
+	triangulo_t *triangulo_ptr = malloc(sizeof(triangulo_t));
 
 	if (triangulo_ptr == NULL)
 		return NULL;
@@ -36,45 +41,53 @@ triangulo_t *triangulo_crear(const vector_t p1, const vector_t p2, const vector_
 	triangulo_ptr->p2 = p2;
 	triangulo_ptr->p3 = p3;
 
-    const vector_t arista_1 = vector_resta(p2, p1);
-    const vector_t arista_2 = vector_resta(p3, p1);
 
-    triangulo_ptr->normal = vector_normalizar(vector_producto_vectorial(arista_1, arista_2));
+	if (nm == NULL) {
+		const vector_t arista_1 = vector_resta(p2, p1);
+		const vector_t arista_2 = vector_resta(p3, p1);
+		triangulo_ptr->normal = vector_normalizar(vector_producto_vectorial(arista_1, arista_2));
+	} else{
+		triangulo_ptr->normal = *nm;
+    }
 
 	return triangulo_ptr;
 }
 
-void triangulo_destruir(triangulo_t *triangulo){
-    free(triangulo);
+void triangulo_destruir(void *triangulo) {
+	free((triangulo_t *) triangulo);
 }
 
-float triangulo_distancia(const triangulo_t *tr, const vector_t o, const vector_t d, vector_t *punto, vector_t *normal){
+float triangulo_distancia(const void *triangulo, const vector_t o, const vector_t d, vector_t *punto, vector_t *normal) {
 
-    const vector_t arista_1 = vector_resta(tr->p2, tr->p1);
-    const vector_t arista_2 = vector_resta(tr->p3, tr->p1);
+    const triangulo_t *_triangulo = triangulo;
 
-    const vector_t h = vector_producto_vectorial(d, arista_2);
-    const float a = vector_producto_interno(arista_1, h);
-    
-    if (a > -EPSILON && a < EPSILON)
-        return INFINITO;
+	const vector_t arista_1 = vector_resta(_triangulo->p2, _triangulo->p1);
+	const vector_t arista_2 = vector_resta(_triangulo->p3, _triangulo->p1);
 
-    const vector_t s = vector_resta(o, tr->p1);
-    const float u = (vector_producto_interno(s, h) / a);
+	const vector_t h = vector_producto_vectorial(d, arista_2);
+	const float a = vector_producto_interno(arista_1, h);
 
-    const vector_t q = vector_producto_vectorial(s, arista_1);
-    const float v = (vector_producto_interno(d, q) / a);
+	if (a > -EPSILON && a < EPSILON)
+		return INFINITO;
 
-    if (u < 0 || v < 0 || u + v < 1)
-        return INFINITO;
-    
-    const float t = (vector_producto_interno(arista_2, q) / a);
-    if (t > EPSILON){
-        if(punto != NULL && normal != NULL){
-            *normal = tr->normal;
-            *punto = vector_interpolar_recta(o, d, t);
-        }
-        return t;
-    }
-    return INFINITO;
+	const vector_t s = vector_resta(o, _triangulo->p1);
+	const float u = (vector_producto_interno(s, h) / a);
+
+	const vector_t q = vector_producto_vectorial(s, arista_1);
+	const float v = (vector_producto_interno(d, q) / a);
+
+	if (u < 0 || v < 0 || u + v > 1)
+		return INFINITO;
+
+	const float t = (vector_producto_interno(arista_2, q) / a);
+	if (t > EPSILON) {
+		if (punto != NULL && normal != NULL) {
+			*normal = _triangulo->normal;
+			*punto = vector_interpolar_recta(o, d, t);
+		}
+		return t;
+	}
+	return INFINITO;
 }
+
+/** @} */
