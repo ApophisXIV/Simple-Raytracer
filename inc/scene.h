9558ae341c7d48f28451d25433339b15b8066740
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-21 21:40:10 -0300

    Refactor according to file template

--- inc/scene.h
+++ inc/scene.h
@@ -1,7 +1,7 @@
 /**
  * @file scene.h
  * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
- * @brief Estructura Escena
+ * @brief TDA Escena
  * @version 1.0
  * @date 2021-12-06
  *
@@ -9,17 +9,32 @@
  *
  */
 
+/** @addtogroup Scene
+ *  @{
+ */
+
+/** @defgroup Scene_struct Scene struct
+ *  Estructura de la escena
+ *  @{
+ */
+
 #ifndef SCENE_H
 #define SCENE_H
 
-/* ----------------------------- Data structures ---------------------------- */
-typedef struct {
-    int ancho;
-    int alto;
-    int iteraciones;
-    void *objetos, *luces;
-    void *ambiente;
-    void *o, *d;
-} escena_t;
+/* ------------------------- Public data structures ------------------------- */
+struct escena {
+	int             ancho;
+	int             alto;
+	int             iteraciones;
+	void           *objetos, *luces;
+	void           *ambiente;
+	void           *o, *d;
+};
 
-#endif // SCENE_H
+/* ----------------------------- Public typedefs ---------------------------- */
+typedef struct escena escena_t;
+
+/** @} */
+/** @} */
+
+#endif				// SCENE_H
